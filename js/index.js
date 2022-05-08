09e86ba01d71cb8c6f4f96c613c9ab2e574b7043
Dolhov Andrey <andrej.dolgov.042110@gmail.com>
2022-05-08 17:01:04 +0300

    done pass generator

--- js/index.js
+++ js/index.js
@@ -1,23 +1,31 @@
 
 
 
-var password=document.getElementById("password");
+let password = document.getElementById("password");
 
- function genPassword() {
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var passwordLength = +prompt('PassLength');
-    var password = "";
- for (var i = 1; i <= passwordLength; i++) {
-   var randomNumber = Math.floor(Math.random() * chars.length);
-   password += chars.substring(randomNumber, randomNumber +1);
-}
+function genPassword() {
+   let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   let passwordLength = +document.getElementById('input__passLenght').value;
+   let password = "";
+   if (passwordLength < 1) {
+      alert('Введите длину пароля!')
+   } else {
+      for (let i = 1; i <= passwordLength; i++) {
+         let randomNumber = Math.floor(Math.random() * chars.length);
+         password += chars.substring(randomNumber, randomNumber + 1);
+      }
+   }
+
+
    document.getElementById('password').value = password;
 }
 
 function copyPassword() {
    var copyText = document.getElementById("password");
    copyText.select();
-   document.execCommand("copy");  
- }
+   document.execCommand("copy");
+}
+
+
 
 
